0ece399facb069d17b32980402419f04dfd73237
unknown <sofoniasmelesse@gmail.com>
2023-11-20 02:13:28 +0300

    12th commit

--- env.mjs
+++ env.mjs
@@ -9,16 +9,16 @@ export const env = createEnv({
   server: {
     // DATABASE_URL: z.string('mongodb+srv://sofoniasmelesse:zPwqM3SNElgoIuWQ@cluster47.4x7suhs.mongodb.net/test?retryWrites=true&w=majority'),
     NODE_ENV: z.enum(["development", "test", "production"]),
-    CLERK_SECRET_KEY: z.string('sk_test_RChscSwKLC1wixP2fYYpjZoHUWqb0QwyQZCIK0pY81'),
+    // CLERK_SECRET_KEY: z.string('sk_test_RChscSwKLC1wixP2fYYpjZoHUWqb0QwyQZCIK0pY81'),
     RESEND_API_KEY: z.string("re_DezGELzD_6k9PMZrR4uNy8dDS3s44RjDH"),
     EMAIL_FROM_ADDRESS: z.string().email("cto@gebeyaw.com"),
     
     // UPLOADTHING_SECRET: z.string("sk_live_630074e7a117be444d0929d1e3d42d3de2f0c02f810cdcff870f4b0047a49bcb"),
     // UPLOADTHING_APP_ID: z.string("pjkq008pqs"),
     
-    POSTGRES_URL: z.string("postgres://default:6pkql7TQhEYi@ep-withered-poetry-08252282-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb"),
+    // POSTGRES_URL: z.string("postgres://default:6pkql7TQhEYi@ep-withered-poetry-08252282-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb"),
     // POSTGRES_PRISMA_URL: z.string("postgres://default:6pkql7TQhEYi@ep-withered-poetry-08252282-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?pgbouncer=true&connect_timeout=15"),
-    POSTGRES_URL_NON_POOLING: z.string("postgres://default:6pkql7TQhEYi@ep-withered-poetry-08252282.us-east-1.postgres.vercel-storage.com:5432/verceldb"),
+    // POSTGRES_URL_NON_POOLING: z.string("postgres://default:6pkql7TQhEYi@ep-withered-poetry-08252282.us-east-1.postgres.vercel-storage.com:5432/verceldb"),
     // POSTGRES_USER: z.string("default"),
     // POSTGRES_HOST: z.string("ep-withered-poetry-08252282-pooler.us-east-1.postgres.vercel-storage.com"),
     // POSTGRES_PASSWORD: z.string("6pkql7TQhEYi"),
@@ -32,7 +32,7 @@ export const env = createEnv({
    */
   client: {
     NEXT_PUBLIC_APP_URL: z.string().url('/'),
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string('pk_test_aG9seS1jYWltYW4tNTcuY2xlcmsuYWNjb3VudHMuZGV2JA'),
+    // NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string('pk_test_aG9seS1jYWltYW4tNTcuY2xlcmsuYWNjb3VudHMuZGV2JA'),
 
   },
 
@@ -43,14 +43,14 @@ export const env = createEnv({
   runtimeEnv: {
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
     // NEXT_PUBLIC_APP_URL: 'http://localhost:3000',
-    // DATABASE_URL: process.env.DATABASE_URL,
+    DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    // CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
 
-    POSTGRES_URL: process.env.POSTGRES_URL,
+    // POSTGRES_URL: process.env.POSTGRES_URL,
     // POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
-    POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING,
+    // POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING,
     // POSTGRES_USER: process.env.POSTGRES_USER,
     // POSTGRES_HOST: process.env.POSTGRES_HOST,
     // POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
@@ -58,8 +58,8 @@ export const env = createEnv({
 
     RESEND_API_KEY: process.env.RESEND_API_KEY,
     EMAIL_FROM_ADDRESS: process.env.EMAIL_FROM_ADDRESS,
-    UPLOADTHING_SECRET: process.env.UPLOADTHING_SECRET,
-    UPLOADTHING_APP_ID: process.env.UPLOADTHING_APP_ID,
+    // UPLOADTHING_SECRET: process.env.UPLOADTHING_SECRET,
+    // UPLOADTHING_APP_ID: process.env.UPLOADTHING_APP_ID,
     // STRIPE_API_KEY: process.env.STRIPE_API_KEY,
     // STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
     // STRIPE_STD_MONTHLY_PRICE_ID: process.env.STRIPE_STD_MONTHLY_PRICE_ID,
